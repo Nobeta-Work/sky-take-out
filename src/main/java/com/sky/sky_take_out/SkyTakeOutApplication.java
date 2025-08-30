9786e8c4b57211f806ca6a676bc0d63421342366
N0beta <377877931@qq.com>
2025-08-31 01:06:44 +0800

    初始化

---
+++ src/main/java/com/sky/sky_take_out/SkyTakeOutApplication.java
@@ -0,0 +1,13 @@
+package com.sky.sky_take_out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyTakeOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyTakeOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/sky_take_out/SkyTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.sky_take_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyTakeOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
