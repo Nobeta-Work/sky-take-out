25c621adc73a95a179ed79f3d84847739ab9ac6f
N0beta <377877931@qq.com>
2025-08-31 02:36:34 +0800

    登录功能->     JWT令牌技术->         1.依赖注入及其配置

---
+++ sky-common/src/main/java/com/sky/properties/JwtProperties.java
@@ -0,0 +1,25 @@
+package com.sky.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private String secret;
+    private Long expiration;
+    // 访问方法
+    public String getSecret() {
+        return secret;
+    }
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+    public Long getExpiration() {
+        return expiration;
+    }
+    public void setExpiration(Long expiration) {
+        this.expiration = expiration;
+    }
+    
+}
--- sky-server/src/main/java/com/sky/SkyTakeOutApplication.java
+++ sky-server/src/main/java/com/sky/SkyTakeOutApplication.java
@@ -2,7 +2,9 @@ package com.sky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties	//启动配置属性绑定
 @SpringBootApplication
 public class SkyTakeOutApplication {
 
